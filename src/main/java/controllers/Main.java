a08f3b4445c025e3428b76ab0c17db4e49e588d5
emy-bit <filali.imane@emsi-edu.ma>
2024-11-15 12:38:54 +0100

    commit the folder ressource

--- src/main/java/controllers/Main.java
+++ src/main/java/controllers/Main.java
@@ -1,22 +1,19 @@
 package controllers;
 
-import javax.swing.*;
+import javax.swing.SwingUtilities;
+
 import vue.EmpruntView;
+import vue.LivreView;
 
 public class Main {
     public static void main(String[] args) {
-        // S'assurer que l'application utilise le Look and Feel de la plateforme
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e) {
-            e.printStackTrace();  // En cas d'erreur, utiliser l'apparence par défaut
-        }
-
-        // Créer et afficher l'interface utilisateur
+        // SwingUtilities.invokeLater garantit que l'interface graphique est créée sur le thread Event Dispatch Thread.
         SwingUtilities.invokeLater(() -> {
-            // Créer l'instance de EmpruntView et l'afficher
+            // Créer une instance de LivreView et la rendre visible
+            LivreView livreView = new LivreView();
+            livreView.setVisible(true);
             EmpruntView empruntView = new EmpruntView();
-            empruntView.setVisible(true); // Afficher la fenêtre des emprunts
+            empruntView.setVisible(true);
         });
     }
 }
